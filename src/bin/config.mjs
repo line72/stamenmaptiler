3603c75df205b78f8b844a8471d1a79f260d9ab8
jascha <jascha@jaeh.at>
2020-11-11 03:07:15 +0100

    src/bin/config.mjs: only load needed tiles

--- src/bin/config.mjs
+++ src/bin/config.mjs
@@ -1,9 +1,20 @@
+//    [48.4, 16.1],
+//    [48.0, 16.7],
+
 export const bounds = {
-  left: 48.5,
-  right: 47.9,
-  top: 16,
-  bottom: 16.8,
+  left: 48.4,
+  right: 48,
+  top: 16.1,
+  bottom: 16.7,
+
+  // wider bounds for scrolling overlaps?
+  // left: 48.5,
+  // right: 47.9,
+  // top: 16,
+  // bottom: 16.8,
 }
+
+
 
 export const zoom = {
   min: 11,
