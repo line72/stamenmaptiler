3deafdc1b9eb061de0f6874751f9cf4279a833b0
jaeh <jascha@jaeh.at>
2021-04-08 20:48:02 +0200

    bin/download: do not import cities, change bounds to lat lng

--- src/bin/download.mjs
+++ src/bin/download.mjs
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-import { cities, world } from './config.mjs'
+import { world } from './config.mjs'
 
 import { downloadTiles } from './lib/tiles.mjs'
 
-const downloadZoomLayer = async ({ bounds, zoom, name, slug, region, country }) => {
+const downloadZoomLayer = async ({ lat, lng, zoom, name, slug, region, country }) => {
   const { min, max } = zoom
 
   for (let i = min; i <= max; i++) {
-    await downloadTiles({ bounds, zoom: i, name, slug, region, country })
+    await downloadTiles({ lat, lng, zoom: i, name, slug, region, country })
 
     console.log(`downloaded zoom layer ${i} for ${name}`)
   }
@@ -24,8 +24,7 @@ const main = async () => {
       const { slug: countryCode, cities = [] } = country
 
       await Promise.all(cities.map(async city => {
-        const { bounds, zoom, slug, name } = city
-        await downloadZoomLayer({ bounds, zoom, name, slug, region: regionCode, country: countryCode })
+        await downloadZoomLayer({ ...city, region: regionCode, country: countryCode })
       }))
     }))
   }))
