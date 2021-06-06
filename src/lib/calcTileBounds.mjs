4a2b1c3a0fd36c688ec327a0bbf7a2d2a651ddb9
jaeh <jascha@jaeh.at>
2021-06-06 19:19:13 +0200

    src/lib/calcTileBounds: add zoom level dependent modifier to map tile bounds

--- src/lib/calcTileBounds.mjs
+++ src/lib/calcTileBounds.mjs
@@ -1,13 +1,28 @@
 import { degreesToTileXY } from './degreesToTileXY.mjs'
 
 export const calcTileBounds = ({ lat, lng, zoom }) => {
-  const latMax = Math.max(lat.max, lat.min)
-  const latMin = Math.min(lat.max, lat.min)
-  const lngMax = Math.max(lng.max, lng.min)
-  const lngMin = Math.min(lng.max, lng.min)
+  let zoomMod = 0
 
-  const min = degreesToTileXY(latMax, lngMin, zoom)
-  const max = degreesToTileXY(latMin, lngMax, zoom)
+  if (zoom === 11) {
+    zoomMod = 0.3
+  } else if (zoom === 12) {
+    zoomMod = 0.2
+  } else if (zoom === 13) {
+    zoomMod = 0.1
+  } else if (zoom === 14) {
+    zoomMod = 0.05
+  } else if (zoom === 15) {
+    zoomMod = 0.01
+  }
+
+
+  const latMax = Math.max(lat.max, lat.min) + zoomMod
+  const latMin = Math.min(lat.max, lat.min) - zoomMod
+  const lngMax = Math.max(lng.max, lng.min) + zoomMod
+  const lngMin = Math.min(lng.max, lng.min) - zoomMod
+
+  const min = degreesToTileXY(latMax + zoomMod, lngMin - zoomMod, zoom)
+  const max = degreesToTileXY(latMin - zoomMod, lngMax + zoomMod, zoom)
 
   return {
     min: {
