76cf407fbb2a410b5004e8a1e5e6a05647617eea
jaeh <jascha@jaeh.at>
2022-07-27 19:11:40 +0200

    src/regions/eu/it: change schlanders lat & lng

--- src/regions/eu/it.mjs
+++ src/regions/eu/it.mjs
@@ -5,16 +5,16 @@ const schlanders = {
   region: 'eu',
 
   lat: {
-    min: 52.4,
-    max: 52.6,
+    min: 46.622,
+    max: 46.633,
   },
   lng: {
-    min: 13.15,
-    max: 13.6,
+    min: 10.7,
+    max: 10.8,
   },
   zoom: {
     min: 11,
-    max: 18,
+    max: 16,
   },
 }
 
