f38a04c36623135a0d95641aff7beeadcc2de359
jaeh <jascha@jaeh.at>
2022-12-26 04:29:10 +0100

    add src/regions/as and india

---
+++ src/regions/as/in.mjs
@@ -0,0 +1,28 @@
+const mumbai = {
+  name: 'Mumbai',
+  slug: 'mumbai',
+  country: 'in',
+  region: 'as',
+  lat: {
+    min: 18.875,
+    max: 19.25,
+  },
+  lng: {
+    min: 72.77,
+    max: 73,
+  },
+  zoom: {
+    min: 11,
+    max: 16,
+  },
+}
+
+export const in_ = {
+  slug: 'in',
+  name: 'India',
+  bounds: {},
+  zoom: {},
+  cities: [
+    mumbai,
+  ],
+}
---
+++ src/regions/as/index.mjs
@@ -0,0 +1,21 @@
+import { in_ } from './in.mjs'
+
+export const as = {
+  name: 'Asia',
+  slug: 'as',
+  // lat: {
+  //   min: 33.33,
+  //   max: 61.53,
+  // },
+  // lng: {
+  //   min: -11.23,
+  //   max: 29.5,
+  // },
+
+  // zoom: {
+  //   min: 6,
+  //   max: 10,
+  // },
+
+  countries: [in_],
+}
